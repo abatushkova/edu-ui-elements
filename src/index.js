5d994f7d688e48b09ae653bb9e44c8c05bc4fb06
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-07 22:41:34 +0300

    add sidebar functionality, remove navbar sticky func

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const navbar = document.querySelector('#nav');
-const header = document.querySelector('#header');
-const topOfNavbar = navbar.offsetTop;
+// const header = document.querySelector('#header');
+// const topOfNavbar = navbar.offsetTop;
+const sidebar = document.querySelector('#sidebar');
+const openSidebarBtn = document.querySelector('#openSidebar');
+const closeSidebarBtn = document.querySelector('#closeSidebar');
 let activeLink = null;
 
 const highlightActiveLink = (link) => {
@@ -21,15 +24,33 @@ const setActiveLink = (e) => {
   highlightActiveLink(target);
 };
 
-const toggleFixedNavbar = () => {
-  if (window.scrollY >= topOfNavbar) {
-    header.style.paddingBottom = `${navbar.offsetHeight}px`;
-    navbar.classList.add('is-fixed');
-  } else {
-    header.style.paddingBottom = 0;
-    navbar.classList.remove('is-fixed');
-  }
+// const toggleFixedNavbar = () => {
+//   if (window.scrollY >= topOfNavbar) {
+//     header.style.paddingBottom = `${navbar.offsetHeight}px`;
+//     navbar.classList.add('is-fixed');
+//   } else {
+//     header.style.paddingBottom = 0;
+//     navbar.classList.remove('is-fixed');
+//   }
+// };
+
+const openSidebar = () => {
+  sidebar.classList.add('is-open');
+};
+
+const closeSidebar = () => {
+  sidebar.classList.remove('is-open');
+}
+
+const closeModal = (e) => {
+  const target = e.target;
+
+  if (target == sidebar) closeSidebar();
+  if (target.closest('#closeSidebar')) closeSidebar();
 };
 
 navbar.addEventListener('click', setActiveLink);
-window.addEventListener('scroll', toggleFixedNavbar);
+// window.addEventListener('scroll', toggleFixedNavbar);
+openSidebarBtn.addEventListener('click', openSidebar);
+closeSidebarBtn.addEventListener('click', closeModal);
+window.addEventListener('click', closeModal);
