bccc0aa6ffed3e00598bfb6f0b8a4530564c965d
Alena Batushkova <alena.batushkova@gmail.com>
2023-01-12 20:59:09 +0300

    fix dark-light theme functionality

--- src/modules/theme.js
+++ src/modules/theme.js
@@ -1,14 +1,38 @@
+import { themeToggler, iconDark, iconLight } from '../utils/constants.js';
 const html = document.documentElement;
 
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  html.setAttribute('data-theme', 'dark');
-} else {
-  html.setAttribute('data-theme', 'light');
+const setThemeIcon = (mode) => {
+  if (mode === 'dark') {
+    iconDark.classList.add('hide');
+    iconLight.classList.remove('hide');
+  } else {
+    iconLight.classList.add('hide');
+    iconDark.classList.remove('hide');
+  }
+};
+
+export const setTheme = () => {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    html.setAttribute('data-theme', 'dark');
+    setThemeIcon('dark');
+    themeToggler.checked = true;
+  } else {
+    html.setAttribute('data-theme', 'light');
+    setThemeIcon('light');
+    themeToggler.checked = false;
+  }
 }
 
 export const toggleTheme = (e) => {
-  html.setAttribute(
-    'data-theme',
-    e.target.checked ? 'dark' : 'light'
-  );
-};
+  let mode = '';
+
+  if (e.target.checked) {
+    mode = 'dark';
+    setThemeIcon('dark');
+  } else {
+    mode = 'light';
+    setThemeIcon('light');
+  }
+
+  html.setAttribute('data-theme', mode);
+}
